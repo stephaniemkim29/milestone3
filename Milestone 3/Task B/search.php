a59f44e184adf0acf1463fd20875e63f2f030d01
stephaniemkim29 <114373893+stephaniemkim29@users.noreply.github.com>
2024-04-14 16:47:23 -0400

    Add files via upload

---
+++ Milestone 3/Task B/search.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fitted</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="search-title-header">
+            <h1>Fitted</h1>
+        </div>
+    </header>
+    <div class="search-container">
+        <h2> Welcome back <?php echo $_POST["name"]; ?> </h2>
+        <h2 class >Search Your Outfit</h2>
+
+
+</body>
+
